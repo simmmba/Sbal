51ffec5804973975e67a1ffba7c335e16448fa95
임선우 <ajtnlaos94@naver.com>
2020-02-10 10:07:41 +0900

    2020-02-10 10:07 SW commit

--- server/src/main/java/com/ssafy/sval/controller/UserController.java
+++ server/src/main/java/com/ssafy/sval/controller/UserController.java
@@ -21,8 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @RestController
 @RequestMapping("/user")
