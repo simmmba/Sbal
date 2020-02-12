cb1df9d6faa8987f481e8f78e1a0c6a7dd08e2ee
simba <jsy9532@gmail.com>
2020-02-12 09:18:41 +0900

    스터디그룹 페이지 진행중

--- client/src/components/studyGroup/StudyGroupMain.tsx
+++ client/src/components/studyGroup/StudyGroupMain.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Display } from '../Display'
+import { Menu, Icon } from 'antd'
 
+// const { SubMenu } = Menu
 const StudyGroupMain = () => {
   const studyinfo = [
     {
@@ -8,7 +10,29 @@ const StudyGroupMain = () => {
       lala: ''
     }
   ]
-  return <Display></Display>
+
+  return (
+    <Display>
+      <Menu
+        // onClick={this.handleClick}
+        // selectedKeys={[this.state.current]}
+        mode="horizontal"
+      >
+        <Menu.Item>
+          <Icon type="schedule" style={{ fontSize: 19 }} theme="twoTone" />
+          일정
+        </Menu.Item>
+        <Menu.Item>
+          <Icon type="snippets" style={{ fontSize: 19 }} theme="twoTone" />
+          게시판
+        </Menu.Item>
+        <Menu.Item>
+          <Icon type="smile" style={{ fontSize: 19 }} theme="twoTone" />
+          멤버 정보
+        </Menu.Item>
+      </Menu>
+    </Display>
+  )
 }
 
 export default StudyGroupMain
