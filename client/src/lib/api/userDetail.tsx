9006093aa8d38fe9c51a1f5a3f982ef69bfbae4b
JunSeong Park <pjs92kr@outlook.com>
2020-02-17 12:38:27 +0900

    checkout branch

--- client/src/lib/api/userDetail.tsx
+++ client/src/lib/api/userDetail.tsx
@@ -1,10 +1,10 @@
 import apiClient from './client'
 import setAuthToken from '../../utils/setAuthToken'
-//import { userInfo } from '../../components/userDetail/UserInfoTypes'
+// import { userInfo } from '../../components/userDetail/UserInfoTypes'
 
 export const userInfo = () => {
   if (sessionStorage.token) {
-    //alert(sessionStorage.token)
+    // alert(sessionStorage.token)
     setAuthToken(sessionStorage.token)
   }
   return apiClient.get('/user/myPage')
