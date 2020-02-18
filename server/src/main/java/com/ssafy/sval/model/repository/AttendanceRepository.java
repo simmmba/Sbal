70c7f14df455b82da2f47fb843402207de9be589
JunSeong Park <pjs92kr@outlook.com>
2020-02-18 17:44:38 +0900

    2020-02-18 17:45 add evaluate func JS Commit

--- server/src/main/java/com/ssafy/sval/model/repository/AttendanceRepository.java
+++ server/src/main/java/com/ssafy/sval/model/repository/AttendanceRepository.java
@@ -2,8 +2,11 @@ package com.ssafy.sval.model.repository;
 
 import com.ssafy.sval.model.entity.Attendance;
 import com.ssafy.sval.model.entity.AttendanceId;
+import com.ssafy.sval.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttendanceRepository extends JpaRepository<Attendance, AttendanceId> {
+import java.util.List;
 
+public interface AttendanceRepository extends JpaRepository<Attendance, AttendanceId> {
+    List<Attendance> findAllByUser(User user);
 }
--- server/src/main/java/com/ssafy/sval/model/service/AttendanceService.java
+++ server/src/main/java/com/ssafy/sval/model/service/AttendanceService.java
@@ -1,18 +1,43 @@
 package com.ssafy.sval.model.service;
 
 import com.ssafy.sval.model.entity.Attendance;
+import com.ssafy.sval.model.entity.User;
 import com.ssafy.sval.model.repository.AttendanceRepository;
+import com.ssafy.sval.model.repository.StudyScheduleRepository;
+import com.ssafy.sval.model.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class AttendanceService {
     @Autowired
     AttendanceRepository attendanceRepository;
+    @Autowired
+    StudyScheduleRepository scheduleRepository;
+    @Autowired
+    UserRepository userRepository;
 
     public void update(Attendance attendance) {
-        attendanceRepository.save(attendance);
+        // 1. attendance 테이블에서 user id 가 일치하는 모든 칼럼을 가져온다.
+        attendance = attendanceRepository.save(attendance);
+        User targetUser = attendance.getUser();
+        List<Attendance> attendanceList = attendanceRepository.findAllByUser(targetUser);
+
+        int sum = 100;
+        int cnt = 1;
+        for (Attendance a : attendanceList) {
+            if(a.getState()!=0) {
+                sum += a.getState();
+                cnt++;
+            }
+        }
+        double avg = (double) sum / cnt;
+        if(cnt == 1) avg = 100.0;
+        targetUser.setEvaluation((double) Math.round(avg));
+        userRepository.save(targetUser);
     }
 }
